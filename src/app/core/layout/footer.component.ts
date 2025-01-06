4fbd0310dd2cf3ab108fdeac0ca8fed8a81a4bca
Sicong Xu <sicong.xu@roseautechnologies.com>
2025-01-06 11:36:03 +0100

    BP<n°27>-<N'utilisez que les portions indispensables des librairies JavaScript et frameworks CSS>

--- src/app/core/layout/footer.component.ts
+++ src/app/core/layout/footer.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from "@angular/core";
 import { DatePipe } from "@angular/common";
 import { RouterLink } from "@angular/router";
 
